d78e787940775928e23de5c40378edb713d7bbd6
Facundo <facundomedica@gmail.com>
2025-03-25 16:59:10 +0100

    use cometbft state directly

--- adapter/store.go
+++ adapter/store.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	cmtstateproto "github.com/cometbft/cometbft/proto/tendermint/state"
+	cmtstate "github.com/cometbft/cometbft/state"
 	cmtypes "github.com/cometbft/cometbft/types"
-	adapterpb "github.com/rollkit/go-execution-abci/adapter/proto"
+	proto "github.com/cosmos/gogoproto/proto"
 	"github.com/rollkit/rollkit/store"
-	"google.golang.org/protobuf/proto"
 )
 
 const stateKey = "abci-s"
@@ -22,86 +23,26 @@ type State struct {
 	LastHeightValidatorsChanged      int64
 }
 
-// ToProto converts the State to a protobuf message
-func (s *State) ToProto() (*adapterpb.StateProto, error) {
-	consensusParamsProto := s.ConsensusParams.ToProto()
-	validatorSetProto, err := s.Validators.ToProto()
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert validators to proto: %w", err)
-	}
-
-	// Convert next validators to proto
-	nextValidatorSetProto, err := s.NextValidators.ToProto()
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert next validators to proto: %w", err)
-	}
-
-	// Convert last validators to proto
-	lastValidatorSetProto, err := s.LastValidators.ToProto()
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert last validators to proto: %w", err)
-	}
-
-	statePb := &adapterpb.StateProto{
-		ConsensusParams:                  &consensusParamsProto,
-		LastHeightConsensusParamsChanged: s.LastHeightConsensusParamsChanged,
-		Validators:                       validatorSetProto,
-		NextValidators:                   nextValidatorSetProto,
-		LastValidators:                   lastValidatorSetProto,
-		LastHeightValidatorsChanged:      s.LastHeightValidatorsChanged,
-	}
-
-	return statePb, nil
-}
-
-// FromProto converts a protobuf message to a State
-func StateFromProto(sp *adapterpb.StateProto) (*State, error) {
-	// Create empty validator sets
-	validators, err := cmtypes.ValidatorSetFromProto(sp.Validators)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert validators to proto: %w", err)
-	}
-	nextValidators, err := cmtypes.ValidatorSetFromProto(sp.NextValidators)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert next validators to proto: %w", err)
-	}
-	lastValidators, err := cmtypes.ValidatorSetFromProto(sp.LastValidators)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert last validators to proto: %w", err)
-	}
-
-	consensusParams := cmtypes.ConsensusParamsFromProto(*sp.ConsensusParams)
-
-	return &State{
-		ConsensusParams:                  consensusParams,
-		LastHeightConsensusParamsChanged: sp.LastHeightConsensusParamsChanged,
-		Validators:                       validators,
-		NextValidators:                   nextValidators,
-		LastValidators:                   lastValidators,
-		LastHeightValidatorsChanged:      sp.LastHeightValidatorsChanged,
-	}, nil
-}
-
 // loadState loads the state from disk
-func loadState(ctx context.Context, s store.Store) (*State, error) {
+func loadState(ctx context.Context, s store.Store) (*cmtstate.State, error) {
 	data, err := s.GetMetadata(ctx, stateKey)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get state metadata: %w", err)
 	}
 	if data == nil {
-		return &State{}, nil
+		return &cmtstate.State{}, nil
 	}
 
-	var stateProto adapterpb.StateProto
-	if err := proto.Unmarshal(data, &stateProto); err != nil {
+	var stateProto *cmtstateproto.State
+	if err := proto.Unmarshal(data, stateProto); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal state: %w", err)
 	}
 
-	return StateFromProto(&stateProto)
+	return cmtstate.FromProto(stateProto)
 }
 
 // saveState saves the state to disk
-func saveState(ctx context.Context, s store.Store, state *State) error {
+func saveState(ctx context.Context, s store.Store, state *cmtstate.State) error {
 	stateProto, err := state.ToProto()
 	if err != nil {
 		return fmt.Errorf("failed to convert state to proto: %w", err)

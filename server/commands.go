d86557645f6a35a6c4c8d8e6145b7ffff5a3823c
Julien Robert <julien@rbrt.fr>
2025-04-22 10:44:50 +0200

    split

--- server/commands.go
+++ server/commands.go
@@ -14,30 +14,35 @@ func InitCmd() *cobra.Command {
 		Use:   "init",
 		Short: "Initialize rollkit configuration files.",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			aggregator, err := cmd.Flags().GetBool(rollconf.FlagAggregator)
-			if err != nil {
-				return fmt.Errorf("error reading aggregator flag: %w", err)
-			}
-
-			// ignore error, as we are creating a new config
-			// we use load in order to parse all the flags
-			cfg, _ := rollconf.Load(cmd)
-			cfg.Node.Aggregator = aggregator
-
-			if err := cfg.Validate(); err != nil {
-				return fmt.Errorf("error validating config: %w", err)
-			}
-
-			if err := cfg.SaveAsYaml(); err != nil {
-				return fmt.Errorf("error writing rollkit.yaml file: %w", err)
-			}
-
-			return nil
-		},
+		RunE:  InitRunE,
 	}
 
 	rollconf.AddFlags(cmd)
 
 	return cmd
+}
+
+// InitRunE is the function that is called when the init command is run.
+// It can be used standalone to wrap the SDK genesis init command.
+// Or used via the InitCmd function to create a new command.
+func InitRunE(cmd *cobra.Command, args []string) error {
+	aggregator, err := cmd.Flags().GetBool(rollconf.FlagAggregator)
+	if err != nil {
+		return fmt.Errorf("error reading aggregator flag: %w", err)
+	}
+
+	// ignore error, as we are creating a new config
+	// we use load in order to parse all the flags
+	cfg, _ := rollconf.Load(cmd)
+	cfg.Node.Aggregator = aggregator
+
+	if err := cfg.Validate(); err != nil {
+		return fmt.Errorf("error validating config: %w", err)
+	}
+
+	if err := cfg.SaveAsYaml(); err != nil {
+		return fmt.Errorf("error writing rollkit.yaml file: %w", err)
+	}
+
+	return nil
 }

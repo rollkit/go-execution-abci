f1b5304e2d997f231f4ef73b49d20070f2afbdb6
Randy Grok <randygrok@gmail.com>
2025-04-28 20:11:21 +0200

    reduce interface to rpc client

--- pkg/rpc/json/service.go
+++ pkg/rpc/json/service.go
@@ -13,7 +13,6 @@ import (
 	"github.com/cometbft/cometbft/libs/log"
 	rpcclient "github.com/cometbft/cometbft/rpc/client"
 	ctypes "github.com/cometbft/cometbft/rpc/core/types"
-	"github.com/cometbft/cometbft/types"
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
 )

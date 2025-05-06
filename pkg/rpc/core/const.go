9681a6d58a28c880123eb434912afebd347c9d13
Randy Grok <randygrok@gmail.com>
2025-05-06 11:58:39 +0200

    remove unused code

--- pkg/rpc/core/const.go
+++ pkg/rpc/core/const.go
@@ -1,7 +1,5 @@
 package core
 
-import "time"
-
 const (
 	// maxQueryLength is the maximum length of a query string that will be
 	// accepted. This is just a safety check to avoid outlandish queries.
@@ -9,14 +7,6 @@ const (
 
 	defaultPerPage = 30
 	maxPerPage     = 100
-
-	// SubscribeTimeout is the maximum time we wait to subscribe for an event.
-	// must be less than the server's write timeout (see rpcserver.DefaultConfig)
-	SubscribeTimeout = 5 * time.Second
-
-	// genesisChunkSize is the maximum size, in bytes, of each
-	// chunk in the genesis structure for the chunked API
-	genesisChunkSize = 16 * 1024 * 1024 // 16
 )
 
 const (

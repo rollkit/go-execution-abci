691590323ea8beef5b875c335af7a038da18f212
Alex Peters <alpe@users.noreply.github.com>
2025-06-24 17:46:21 +0200

    Start better attestation

--- modules/network/keeper/msg_server_test.go
+++ modules/network/keeper/msg_server_test.go
@@ -2,16 +2,27 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	cmttypes "github.com/cometbft/cometbft/types"
 	"maps"
 	"slices"
 	"strings"
 	"testing"
 	"time"
 
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+
+	"github.com/cosmos/gogoproto/proto"
+	ds "github.com/ipfs/go-datastore"
+	kt "github.com/ipfs/go-datastore/keytransform"
+	rollnode "github.com/rollkit/rollkit/node"
+	rstore "github.com/rollkit/rollkit/pkg/store"
+
 	"cosmossdk.io/log"
 	"cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	"github.com/cosmos/cosmos-sdk/runtime"
 	"github.com/cosmos/cosmos-sdk/testutil/integration"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -19,6 +30,7 @@ import (
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	rollkittypes "github.com/rollkit/rollkit/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -26,7 +38,7 @@ import (
 )
 
 func TestJoinAttesterSet(t *testing.T) {
-	myValAddr := sdk.ValAddress("validator4")
+	myValAddr := sdk.ValAddress("validator")
 
 	type testCase struct {
 		setup  func(t *testing.T, ctx sdk.Context, keeper *Keeper, sk *MockStakingKeeper)
@@ -114,7 +126,7 @@ func TestJoinAttesterSet(t *testing.T) {
 			logger := log.NewTestLogger(t)
 			cms := integration.CreateMultiStore(keys, logger)
 			authority := authtypes.NewModuleAddress("gov")
-			keeper := NewKeeper(cdc, runtime.NewKVStoreService(keys[types.StoreKey]), sk, nil, nil, authority.String())
+			keeper := NewKeeper(cdc, runtime.NewKVStoreService(keys[types.StoreKey]), sk, nil, nil, nil, authority.String())
 			server := msgServer{Keeper: keeper}
 			ctx := sdk.NewContext(cms, cmtproto.Header{ChainID: "test-chain", Time: time.Now().UTC(), Height: 10}, false, logger).
 				WithContext(t.Context())
@@ -141,23 +153,192 @@ func TestJoinAttesterSet(t *testing.T) {
 	}
 }
 
+func TestAttest(t *testing.T) {
+	var (
+		myHash     = sha256.Sum256([]byte("app_hash"))
+		myAppHash  = myHash[:]
+		chainID    = "testing"
+		voteSigner = ed25519.GenPrivKey()
+		validVote  = &cmtproto.Vote{
+			Type:             cmtproto.PrecommitType,
+			Height:           10,
+			Round:            0,
+			BlockID:          cmtproto.BlockID{Hash: myAppHash, PartSetHeader: cmtproto.PartSetHeader{Total: 1, Hash: myAppHash}},
+			Timestamp:        time.Now(),
+			ValidatorAddress: voteSigner.PubKey().Address(),
+			ValidatorIndex:   0,
+		}
+		// Create the vote signature using VoteSignBytes
+	)
+	validVote.Signature = must(voteSigner.Sign(cmttypes.VoteSignBytes(chainID, validVote)))
+	validVoteBz := must(proto.Marshal(validVote))
+
+	testCases := map[string]struct {
+		setupMock func(t *testing.T, ctx sdk.Context, keeper *Keeper, sk *MockStakingKeeper, bs rstore.Store)
+		msg       *types.MsgAttest
+		expErr    error
+	}{
+		"valid attestation": {
+			setupMock: func(t *testing.T, ctx sdk.Context, keeper *Keeper, sk *MockStakingKeeper, bs rstore.Store) {
+				// Use the validator address from the vote
+				valAddrStr := sdk.ValAddress(voteSigner.PubKey().Address()).String()
+				validator, err := stakingtypes.NewValidator(valAddrStr, voteSigner.PubKey(), stakingtypes.Description{})
+				require.NoError(t, err)
+				require.NoError(t, sk.SetValidator(ctx, validator))
+				require.NoError(t, keeper.SetAttesterSetMember(ctx, valAddrStr))
+				require.NoError(t, keeper.SetValidatorIndex(ctx, valAddrStr, 0, 100))
+				sk.SetValidatorPubKey(valAddrStr, voteSigner.PubKey())
+
+				// Set up params for checkpoint height
+				params := types.DefaultParams()
+				params.EpochLength = 10
+				require.NoError(t, keeper.SetParams(ctx, params))
+
+				header := rollkittypes.Header{
+					BaseHeader: rollkittypes.BaseHeader{
+						Height:  10,
+						Time:    uint64(time.Now().UnixNano()),
+						ChainID: "testing",
+					},
+					Version:         rollkittypes.Version{Block: 1, App: 1},
+					ProposerAddress: voteSigner.PubKey().Address(),
+					AppHash:         myAppHash,
+					DataHash:        []byte("data_hash"),
+					ConsensusHash:   []byte("consensus_hash"),
+					ValidatorHash:   []byte("validator_hash"),
+				}
+				signedHeader := &rollkittypes.SignedHeader{
+					Header:    header,
+					Signature: myAppHash,
+					//Signer:    rollkittypes.Signer{PubKey: voteSigner.PubKey()},
+				}
+				data := &rollkittypes.Data{
+					Txs: rollkittypes.Txs{},
+				}
+				var signature rollkittypes.Signature
+				require.NoError(t, bs.SaveBlockData(ctx, signedHeader, data, &signature))
+			},
+			msg: &types.MsgAttest{
+				Validator: sdk.ValAddress(voteSigner.PubKey().Address()).String(),
+				Height:    10,
+				Vote:      validVoteBz,
+			},
+		},
+		//"validator not in attester set": {
+		//	setupMock: func(t *testing.T, ctx sdk.Context, keeper *Keeper, sk *MockStakingKeeper, bs rstore.Store) {
+		//		// Set up validator but don't add to attester set
+		//		valAddr := sdk.ValAddress("validator1")
+		//		validator := stakingtypes.Validator{
+		//			OperatorAddress: valAddr.String(),
+		//			Status:          stakingtypes.Bonded,
+		//		}
+		//		require.NoError(t, sk.SetValidator(ctx, validator))
+		//
+		//		// Set up params for checkpoint height
+		//		params := types.DefaultParams()
+		//		params.EpochLength = 10 // Make height 10 a checkpoint
+		//		require.NoError(t, keeper.SetParams(ctx, params))
+		//	},
+		//	msg: &types.MsgAttest{
+		//		Validator: sdk.ValAddress("validator1").String(),
+		//		Height:    10,
+		//		Vote:      []byte("mock_vote"),
+		//	},
+		//	expErr: sdkerrors.ErrUnauthorized,
+		//},
+		//"not a checkpoint height": {
+		//	setupMock: func(t *testing.T, ctx sdk.Context, keeper *Keeper, sk *MockStakingKeeper, bs rstore.Store) {
+		//		// Set up validator and add to attester set
+		//		valAddr := sdk.ValAddress("validator1")
+		//		validator := stakingtypes.Validator{
+		//			OperatorAddress: valAddr.String(),
+		//			Status:          stakingtypes.Bonded,
+		//		}
+		//		require.NoError(t, sk.SetValidator(ctx, validator))
+		//		require.NoError(t, keeper.SetAttesterSetMember(ctx, valAddr.String()))
+		//
+		//		// Set up params for checkpoint height
+		//		params := types.DefaultParams()
+		//		params.EpochLength = 10 // Make height 10 a checkpoint
+		//		require.NoError(t, keeper.SetParams(ctx, params))
+		//	},
+		//	msg: &types.MsgAttest{
+		//		Validator: sdk.ValAddress("validator1").String(),
+		//		Height:    11, // Not a checkpoint height
+		//		Vote:      []byte("mock_vote"),
+		//	},
+		//	expErr: sdkerrors.ErrInvalidRequest,
+		//},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// Create mocks with test doubles
+			sk := NewMockStakingKeeper()
+
+			rollkitPrefixStore := kt.Wrap(ds.NewMapDatastore(), &kt.PrefixTransform{
+				Prefix: ds.NewKey(rollnode.RollkitPrefix),
+			})
+			bs := rstore.New(rollkitPrefixStore)
+
+			// Set up codec and store
+			cdc := moduletestutil.MakeTestEncodingConfig().Codec
+			keys := storetypes.NewKVStoreKeys(types.StoreKey)
+
+			// Set up context and keeper
+			logger := log.NewTestLogger(t)
+			cms := integration.CreateMultiStore(keys, logger)
+			authority := authtypes.NewModuleAddress("gov")
+			keeper := NewKeeper(cdc, runtime.NewKVStoreService(keys[types.StoreKey]), &sk, nil, nil, bs, authority.String())
+			server := NewMsgServerImpl(keeper)
+
+			ctx := sdk.NewContext(cms, cmtproto.Header{ChainID: "test-chain", Time: time.Now().UTC(), Height: 20}, false, logger).
+				WithContext(t.Context())
+
+			// Run setup
+			tc.setupMock(t, ctx, &keeper, &sk, bs)
+
+			// when
+			rsp, err := server.Attest(ctx, tc.msg)
+
+			// then
+			if tc.expErr != nil {
+				require.Error(t, err)
+				require.ErrorIs(t, err, tc.expErr)
+				require.Nil(t, rsp)
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, rsp)
+
+			// In a successful case, we would check that the signature was stored
+			// and the attestation bitmap was updated
+			// But since we're mocking the verifyVote method, we can't check these
+			// in a meaningful way
+		})
+	}
+}
+
 var _ types.StakingKeeper = &MockStakingKeeper{}
 
 type MockStakingKeeper struct {
 	activeSet map[string]stakingtypes.Validator
+	pubKeys   map[string]cryptotypes.PubKey
 }
 
 func NewMockStakingKeeper() MockStakingKeeper {
 	return MockStakingKeeper{
 		activeSet: make(map[string]stakingtypes.Validator),
+		pubKeys:   make(map[string]cryptotypes.PubKey),
 	}
 }
 
 func (m *MockStakingKeeper) SetValidator(ctx context.Context, validator stakingtypes.Validator) error {
 	m.activeSet[validator.GetOperator()] = validator
 	return nil
-
 }
+
 func (m MockStakingKeeper) GetAllValidators(ctx context.Context) (validators []stakingtypes.Validator, err error) {
 	return slices.SortedFunc(maps.Values(m.activeSet), func(v1 stakingtypes.Validator, v2 stakingtypes.Validator) int {
 		return strings.Compare(v1.OperatorAddress, v2.OperatorAddress)
@@ -165,11 +346,24 @@ func (m MockStakingKeeper) GetAllValidators(ctx context.Context) (validators []s
 }
 
 func (m MockStakingKeeper) GetValidator(ctx context.Context, addr sdk.ValAddress) (validator stakingtypes.Validator, err error) {
+	// First try to find the validator by address
 	validator, found := m.activeSet[addr.String()]
-	if !found {
-		return validator, sdkerrors.ErrNotFound
+	if found {
+		return validator, nil
 	}
-	return validator, nil
+
+	// If not found by address, try to find by public key address
+	addrStr := addr.String()
+	for valAddrStr, pubKey := range m.pubKeys {
+		if pubKey.Address().String() == addrStr {
+			validator, found = m.activeSet[valAddrStr]
+			if found {
+				return validator, nil
+			}
+		}
+	}
+
+	return validator, sdkerrors.ErrNotFound
 }
 
 func (m MockStakingKeeper) GetLastValidators(ctx context.Context) (validators []stakingtypes.Validator, err error) {
@@ -183,4 +377,15 @@ func (m MockStakingKeeper) GetLastValidators(ctx context.Context) (validators []
 
 func (m MockStakingKeeper) GetLastTotalPower(ctx context.Context) (math.Int, error) {
 	return math.NewInt(int64(len(m.activeSet))), nil
+}
+
+func (m *MockStakingKeeper) SetValidatorPubKey(valAddrStr string, key cryptotypes.PubKey) {
+	m.pubKeys[valAddrStr] = key
+}
+
+func must[T any](r T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return r
 }
